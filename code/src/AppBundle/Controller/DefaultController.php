c0ff613339b5547ead70bb965de65741154ddc86
acanimal <asantiagop@gmail.com>
2017-11-22 22:52:17 +0100

    Creating endpoint for phystrix-dashboard

--- code/src/AppBundle/Controller/DefaultController.php
+++ code/src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Service\FindProductsService;
+use AppBundle\Phystrix\PhystrixConfig;
+use Zend\Config\Config;
+use Odesk\PhystrixDashboard\MetricsEventStream\ApcMetricsPoller;
+use Odesk\PhystrixDashboard\MetricsEventStream\MetricsServer;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class DefaultController extends Controller
 {
@@ -23,7 +28,12 @@ class DefaultController extends Controller
      * @Route("/hystrix/status", name="hystrix_status")
      */
     public function hystrixStatusAction(Request $request)
-    {
+    {   
+        $config = new Config(PhystrixConfig::config());
+        $metricsPoller = new ApcMetricsPoller($config);
+        $metricsServer = new MetricsServer($metricsPoller);
 
+        ob_flush();
+        $metricsServer->run();
     }
 }
